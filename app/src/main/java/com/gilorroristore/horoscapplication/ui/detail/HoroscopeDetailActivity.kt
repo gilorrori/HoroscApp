c69fb9d1c7dde10d8ddfb0640feb131a127ed52a
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-01-27 09:32:27 -0600

    Se integra la carga de imagen al abrir el detalle del horoscopo

--- app/src/main/java/com/gilorroristore/horoscapplication/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/gilorroristore/horoscapplication/ui/detail/HoroscopeDetailActivity.kt
@@ -8,19 +8,29 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
-import com.bumptech.glide.Glide
 import com.gilorroristore.horoscapplication.R
 import com.gilorroristore.horoscapplication.databinding.ActivityHoroscopeDetailBinding
-import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Aquarius
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Aries
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Cancer
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Capricorn
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Gemini
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Leo
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Libra
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Pisces
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Sagittarius
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Scorpio
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Taurus
+import com.gilorroristore.horoscapplication.domain.model.HoroscopeModel.Virgo
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-import kotlin.getValue
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
     private val horoscopeDetailViewModel by viewModels<HoroscopeDetailViewModel>()
     private lateinit var binding: ActivityHoroscopeDetailBinding
     private val args: HoroscopeDetailActivityArgs by navArgs()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
@@ -64,24 +74,24 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         binding.pb.isVisible = true
     }
 
-    private fun stateSuccess(success: HoroscopeDetailState.Success) {
+    private fun stateSuccess(successHoroscope: HoroscopeDetailState.Success) {
         binding.pb.isVisible = false
-        binding.tvTitle.text = success.sign
-        binding.tvBody.text = success.prediction
+        binding.tvTitle.text = successHoroscope.sign
+        binding.tvBody.text = successHoroscope.prediction
         //Glide.with(this).load(success.icon).into(binding.ivDetail)
-        val image = when (success.horoscopeModel) {
-            HoroscopeModel.Aries -> R.drawable.detail_aries
-            HoroscopeModel.Gemini -> R.drawable.detail_gemini
-            HoroscopeModel.Taurus -> R.drawable.detail_taurus
-            HoroscopeModel.Cancer -> R.drawable.detail_cancer
-            HoroscopeModel.Leo -> R.drawable.detail_leo
-            HoroscopeModel.Virgo -> R.drawable.detail_virgo
-            HoroscopeModel.Libra -> R.drawable.detail_libra
-            HoroscopeModel.Scorpio -> R.drawable.detail_scorpio
-            HoroscopeModel.Sagittarius -> R.drawable.detail_sagittarius
-            HoroscopeModel.Capricorn -> R.drawable.detail_capricorn
-            HoroscopeModel.Aquarius -> R.drawable.detail_aquarius
-            HoroscopeModel.Pisces -> R.drawable.detail_pisces
+        val image = when (successHoroscope.horoscopeModel) {
+            Aries -> R.drawable.detail_aries
+            Gemini -> R.drawable.detail_gemini
+            Taurus -> R.drawable.detail_taurus
+            Cancer -> R.drawable.detail_cancer
+            Leo -> R.drawable.detail_leo
+            Virgo -> R.drawable.detail_virgo
+            Libra -> R.drawable.detail_libra
+            Scorpio -> R.drawable.detail_scorpio
+            Sagittarius -> R.drawable.detail_sagittarius
+            Capricorn -> R.drawable.detail_capricorn
+            Aquarius -> R.drawable.detail_aquarius
+            Pisces -> R.drawable.detail_pisces
         }
         binding.ivDetail.setImageResource(image)
     }
